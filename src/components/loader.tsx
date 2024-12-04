5c343e8c37cb2aacd55e9cf051c685d7c90cce55
RikyRich09 <riccardoricciardi2009@gmail.com>
2024-12-04 23:50:04 +0100

    Quick Push

---
+++ src/components/loader.tsx
@@ -0,0 +1,8 @@
+export default function Loader() {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full border-blue-600 border-t-transparent" role="status"></div>
+      </div>
+    );
+  }
+  
