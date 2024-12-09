10836aecc1f27ccdb4eb285006bd1f8cf9201b2a
RikyRich09 <105245406+RikyRich09@users.noreply.github.com>
2024-12-09 16:03:52 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="flex items-center justify-center h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white">
       <div className="text-center">
         <h1 className="text-5xl font-extrabold mb-4">Sito in Arrivo!</h1>
-        <p className="text-xl mb-6">Stiamo preparando qualcosa di speciale per te. Ritorna presto!</p>
+        <p className="text-xl mb-6 text-pretty">Stiamo preparando qualcosa di speciale per te. Ritorna presto!</p>
       </div>
     </div>
   );
